ac96c42808d55cb31f9f41a0fbae01f26421070e
mirthful-concept-6030 <jchristinem@gmail.com>
2020-07-30 21:45:25 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,9 @@
 class Transfer
 
-  def initialize(sender)
-    @sender= sender
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
   end
 end
